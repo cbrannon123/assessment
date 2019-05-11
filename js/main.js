0d5f435b9f83856ad2697263fcd85e0a115e7cb4
Casey Brannon <cbrannon412@gmail.com>
2019-05-10 18:41:14 -0700

    Fixing logic issue/finnishing style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+
 var total = [];
+
 
 var inpt1 = document.getElementById('input1');
 var output = document.getElementById('output');
@@ -9,24 +11,40 @@ sum.addEventListener('click', getTotal);
 subtract.addEventListener('click', getNeg)
 
 
+
 function getTotal() {
-        total.push(parseInt(inpt1.value));  
+        total.push(parseInt(inpt1.value)); 
         output.innerHTML = total.reduce(function(sum1, sum2) {
-           return sum1 + sum2;
+            return sum1 + sum2;
         });
+        if(output.innerHTML > 0) {
+            output.style.color = "black"
+        }
+        
+        
         
        
     }
 
     function getNeg() {
-        total.push(parseInt(inpt1.value));
+        total.push(parseInt(inpt1.value * -1));
+        console.log(total)
         output.innerHTML -= input1.value;
+        if(output.innerHTML < 0) {
+            output.style.color = "red";
+        }
+        
 
-        if(output.innerHTML < 0){
-         output.style.color = "red"
-        };
+        // var sum1 = 0;
+        // evt.preventDefault();
+        //     total.push(parseInt(inpt1.value * -1)); 
+        //     console.log(total)
+        //     var newT = total.reduce(function(sum1, sum2) {
+        //         return sum1 + sum2;
 
-
-    }
-
-    
+        // if(output.innerHTML < 0){
+        //  output.style.color = "red"
+        // };
+    // });
+    // output.innerHTML = newT;
+}
